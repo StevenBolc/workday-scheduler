d82d4340f0cb872199c217018d11acee171f2d9b
StevenBolc <steven.bolcar95@gmail.com>
2022-07-03 18:18:28 -0400

    added readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 
 setInterval(() => {
-    var time = moment().format("MMMM Do YYYY, h:mm:ss");
-    $("#time").text(time);
-},1000)
+  var time = moment().format("MMMM Do YYYY, h:mm:ss");
+  $("#time").text(time);
+}, 1000)
 
-function handleProjectFormSubmit(event){
-    event.preventDefault();
+function handleProjectFormSubmit(event) {
+  event.preventDefault();
 }
 
 $(document).ready(function () {
- 
+
   let currentHour = moment().format("HH");
   //grabs hour from each time slot and compares it to actual time
   $(".time-div").each(function () {
     var timeDiv = $(this).attr("id").split("-")[1];
-    
+
     if (currentHour == timeDiv) {
       $(this).addClass("present");
       $(this).children(".description").addClass("white-text");
@@ -26,13 +26,6 @@ $(document).ready(function () {
       $(this).addClass("past");
     }
   });
- // adds functionality to the save button
-  $(".saveBtn").click(function (event) {
-      event.preventDefault();
-      var value = $(this).siblings(".time-block").val();
-      var time = $(this).parent().attr("id").split("-")[1];
-      localStorage.setItem(time, value);
-    });
 
   //retrieves items from local storage and sets them in proper places
   $("#hour-09 .time-block").val(localStorage.getItem("09"));
@@ -44,4 +37,14 @@ $(document).ready(function () {
   $("#hour-3 .time-block").val(localStorage.getItem("3"));
   $("#hour-4 .time-block").val(localStorage.getItem("4"));
   $("#hour-5 .time-block").val(localStorage.getItem("5"));
+
+  // adds functionality to the save button
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
+
+
 });
